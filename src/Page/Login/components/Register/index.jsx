dc8c5def79b9e559a5bb699f8353e144e06423f9
Trực Lê <lechinhtruc999@gmail.com>
2023-02-20 23:29:57 +0700

    update

--- src/Page/Login/components/Register/index.jsx
+++ src/Page/Login/components/Register/index.jsx
@@ -9,7 +9,6 @@ import {
 
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 export default function RegisterUI(props) {
   const [email, setEmail] = useState("");
